896fe0c92f4a743efa97f193604d57890fc8c9ad
lcrbneves <luis.neves1984@gmail.com>
2023-10-04 17:26:05 +0100

    initial commit

--- src/main/java/com/nleiloes/ping/controllers/PingController.java
+++ src/main/java/com/nleiloes/ping/controllers/PingController.java
@@ -1,14 +1,12 @@
-package pt.tml.plannedoffer.controllers;
+package com.nleiloes.ping.controllers;
 
 import lombok.extern.flogger.Flogger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
-import pt.tml.plannedoffer.entities.Webhook;
-import pt.tml.plannedoffer.services.AuthService;
-import pt.tml.plannedoffer.services.OperatorWebClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Flogger
 @RestController
 @CrossOrigin("*")
 @RequestMapping
@@ -21,41 +19,8 @@ public class PingController
     @Value("${info.app.description:unknown}")
     String serviceDescription;
 
-    @Autowired
-    OperatorWebClient operatorWebClient;
-
-    @Autowired
-    AuthService authService;
-
-
-
     @GetMapping("ping")
-    public String ping( /*@RequestHeader(name="Authorization") String token*/ ) {
-
-//        var decoder= new JwtDecoder();
-//        var userName = decoder.extractUsernameFromToken(token);
-//        var roles = decoder.extractRealAccessRolesFromToken(token);
-
+    public String ping() {
         return  String.format("Pong from %s version: %s - %s", serviceName, version, serviceDescription);
     }
-
-
-    @GetMapping("webhookTest")
-    public Webhook webhookTest()
-    {
-
-        var webhook= operatorWebClient.getOperatorWebhook(41, "Plano de Operações");
-        return webhook;
-    }
-
-    @GetMapping("tokenTest")
-    public String tokenTest()
-    {
-        var webhookDefs= operatorWebClient.getOperatorWebhook(41, "Ack Plano de Operação");
-        log.atInfo().log("Obtained %s as post to Operator endpoint", webhookDefs.getWebhookUri());
-
-        var token= authService.getToken(webhookDefs, false);
-        return token;
-    }
-
 }
