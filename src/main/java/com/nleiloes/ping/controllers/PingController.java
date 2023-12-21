c669a1dffae4dba2b0c12ff0cb038cb0bbe230ef
nleiloes <luis.neves1984@gmail.com>
2023-12-21 13:34:04 +0000

    pooooong

--- src/main/java/com/nleiloes/ping/controllers/PingController.java
+++ src/main/java/com/nleiloes/ping/controllers/PingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
 @RestController
@@ -27,7 +28,8 @@ public class PingController
     }
 
     @GetMapping("secured")
-    public String pong(Principal principal) {
-        return  String.format("I'm a secured route: "+ principal.getName().toString());
+    public String pong(HttpServletRequest request) {
+        request.getHeader("Authorization");
+        return  String.format("I'm a secured route: ");
     }
 }
